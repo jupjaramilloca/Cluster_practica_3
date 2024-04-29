395cf302d30c3cccc3a2e8f36d723923e18dc5a9
Juan Pablo Jaramillo Castrillon <jupjaramilloca@unal.edu.co>
2024-04-28 23:06:39 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,8 @@
 @echo off
 
-:: Cambiar al directorio del repositorio clonado
+:: Script de configuración
+
+:: Cambiar al directorio del script
 cd /d %~dp0
 
 :: Ejecutar el script de configuración (setup.py)
@@ -10,6 +12,11 @@ python setup.py
 :: Desconectar el repositorio remoto
 git remote rm origin
 echo Repositorio remoto desconectado.
+
+:: Eliminar la carpeta Setup
+echo Eliminando la carpeta Setup...
+rmdir /s /q "%cd%\Setup"
+echo Carpeta Setup eliminada.
 
 echo Configuración completada.
 pause

93cadcb43db54914d291ae9601b8ec8f613c1093
Juan Pablo Jaramillo Castrillon <jupjaramilloca@unal.edu.co>
2024-04-28 22:39:03 -0500

    V1

--- setup.bat
+++ setup.bat
@@ -1,3 +1,13 @@
 @echo off
+
+:: Script de configuración
+
+:: Cambiar al directorio del script
 cd /d %~dp0
+
+:: Ejecutar el script de configuración (setup.py)
+echo Ejecutando script de configuración...
 python setup.py
+
+echo Configuración completada.
+pause
